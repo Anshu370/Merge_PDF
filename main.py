f333e187381028d171bb4dc9995cc11ae4c310b5
Anshu Gupta <77850344+Anshu370@users.noreply.github.com>
2023-12-27 16:16:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import PyPDF2
+# import sys
+import os
+
+merger = PyPDF2.PdfMerger()
+
+for file in os.listdir(os.curdir):
+    if file.endswith('.pdf'):
+        merger.append(file)
+
+    merger.write('CombinedPDF.pdf')
